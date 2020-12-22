975b05ccef100614dfdddb1eee702da220b6ee13
呂柏賢 <pohsien@mount.rstn.com.tw>
2020-12-22 12:30:52 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formproject';
+  constructor(private formBuilder: FormBuilder) { }
+  ngOnInit() {
+    this.setPhoneValidation();
+  }
+  registerForm: FormGroup = this.formBuilder.group({
+    fullName: [, { validators: [Validators.required], updateOn: "change" }],
+    email: [, {
+      validators: [Validators.required, Validators.email],
+      updateOn: "change",
+    }],
+    password: [, { validators: [Validators.required], updateOn: "change" }],
+    phone: [, { updateOn: "change" }],
+    role: ['jobSeeker', { validators: [Validators.required], updateOn: 'change' }],
+  });
+
+
+
+  setPhoneValidation() {
+    const phoneControl = this.registerForm.get("phone")!;
+    phoneControl.setValidators([Validators.pattern("^[0-9]*$"),
+    Validators.required,]);
+    this.registerForm.get("role")!.valueChanges.subscribe((role) => {
+      if (role == "jobSeeker") {
+        phoneControl.setValidators([Validators.pattern("^[0-9]*$"),
+        Validators.required,]);
+      } else if (role == "employee") {
+        phoneControl.setValidators([Validators.pattern("^[0-9]*$")]);
+      }
+      phoneControl.updateValueAndValidity();
+    });
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatRadioModule} from '@angular/material/radio';
+import { MatCardModule} from '@angular/material/card';
+import { MatInputModule} from '@angular/material/input';
+import { MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatRadioModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
